581a58ef7cd53f6474c7ec8b4d5f3e52eb3ec045
Nataliia Frank <nataliiafrank@gmail.com>
2020-05-28 14:10:52 -0700

    add slider and update sass compiling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import Swiper from 'swiper';
+
 // Defining global veriables
 const bp = {
 	mobile: 320,
@@ -18,6 +20,9 @@ const domSelectors = {
 	features: document.querySelector('.js-features'),
 	featuresButtons: document.querySelector('.js-features-buttons'),
 	featuresButton: document.querySelectorAll('.js-features-button'),
+	swiperContainer: document.querySelector('.swiper-container'),
+	swiperButtonLeft: document.querySelector('.swiper-button-prev'),
+	swiperButtonRight: document.querySelector('.swiper-button-next'),
 }
 
 
@@ -26,11 +31,11 @@ const featuresScroll = () => {
 	const btnsArr = Array.from(domSelectors.featuresButton);
 
 	btnsArr.forEach(item => {
-		if(item.classList.contains(state.active)) {
+		if (item.classList.contains(state.active)) {
 			let itemOffset = item.offsetLeft - 180;
 			//set the scroll to the active button
 
-			if(windowWidth < bp.tablet) {
+			if (windowWidth < bp.tablet) {
 				domSelectors.featuresButtons.scrollLeft = itemOffset;
 			} else {
 				domSelectors.featuresButtons.scrollLeft = 0;
@@ -39,7 +44,27 @@ const featuresScroll = () => {
 	});
 }
 
+const swiper = new Swiper(domSelectors.swiperContainer, {
+	speed: 400,
+	initialSlide: 1,
+	slidesPerView: 'auto',
+	spaceBetween: 15,
+	centeredSlides: true,
+	loop: true,
+	centeredSlidesBounds: true,
+	navigation: {
+		nextEl: domSelectors.swiperButtonRight,
+		prevEl: domSelectors.swiperButtonLeft,
+	},
+	breakpoints: {
+		// when window width is >= 768px
+		768: {
+			spaceBetween: 74,
+		}
+	}
+});
 
 /// Event Listeners
+// Features
 window.addEventListener('load', featuresScroll);
 window.addEventListener('resize', featuresScroll);
