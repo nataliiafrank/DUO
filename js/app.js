bbcb86fc4dfbae254bf1d59f4b4b3c10d6b54e54
Nataliia Frank <nataliiafrank@gmail.com>
2020-05-29 11:03:10 -0700

    testimonial section markup

--- js/app.js
+++ js/app.js
@@ -20,9 +20,11 @@ const domSelectors = {
 	features: document.querySelector('.js-features'),
 	featuresButtons: document.querySelector('.js-features-buttons'),
 	featuresButton: document.querySelectorAll('.js-features-button'),
-	swiperContainer: document.querySelector('.swiper-container'),
-	swiperButtonLeft: document.querySelector('.swiper-button-prev'),
-	swiperButtonRight: document.querySelector('.swiper-button-next'),
+	gallerySwiperContainer: document.querySelector('.js-gallery-slider'),
+	gallerySwiperButtonLeft: document.querySelector('.js-gallery-slider-prev'),
+	gallerySwiperButtonRight: document.querySelector('.js-gallery-slider-next'),
+	testimonialSwiperContainer: document.querySelector('.js-testimonial-slider'),
+	testimonialSwiperPagination: document.querySelector('.js-testimonial-slider-pagination'),
 }
 
 
@@ -44,7 +46,9 @@ const featuresScroll = () => {
 	});
 }
 
-const swiper = new Swiper(domSelectors.swiperContainer, {
+const sliderInit = (container, options) => new Swiper(container, options);
+
+const gallerySliderOptions = {
 	speed: 400,
 	initialSlide: 1,
 	slidesPerView: 'auto',
@@ -53,16 +57,35 @@ const swiper = new Swiper(domSelectors.swiperContainer, {
 	loop: true,
 	centeredSlidesBounds: true,
 	navigation: {
-		nextEl: domSelectors.swiperButtonRight,
-		prevEl: domSelectors.swiperButtonLeft,
+		nextEl: domSelectors.gallerySwiperButtonRight,
+		prevEl: domSelectors.gallerySwiperButtonLeft,
 	},
 	breakpoints: {
 		// when window width is >= 768px
 		768: {
 			spaceBetween: 74,
-		}
+			keyboard: {
+				enabled: true,
+			},
+		},
 	}
-});
+}
+
+const testimonialSliderOptions = {
+	speed: 400,
+	slidesPerView: 1,
+	centeredSlides: true,
+	loop: true,
+	centeredSlidesBounds: true,
+	autoHeight: true,
+	pagination: {
+		el: domSelectors.testimonialSwiperPagination,
+		clickable: true,
+	},
+}
+
+const gallerySlider = sliderInit(domSelectors.gallerySwiperContainer, gallerySliderOptions);
+const testimonialSlider = sliderInit(domSelectors.testimonialSwiperContainer, testimonialSliderOptions);
 
 /// Event Listeners
 // Features
