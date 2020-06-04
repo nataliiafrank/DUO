83aab87e0418e1b3aff958bc8339936b4cf9e9ce
Nataliia Frank <nataliiafrank@gmail.com>
2020-06-04 14:24:09 -0700

    add simple form validation

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@ const windowHeight = window.innerHeight;
 
 const state = {
 	active: 'is-active',
-	desabled: 'is-disabled'
+	desabled: 'is-disabled',
+	valid: 'is-valid',
+	invalid: 'is-invalid'
 }
 
 const domSelectors = {
@@ -26,6 +28,10 @@ const domSelectors = {
 	testimonialSwiperContainer: document.querySelector('.js-testimonial-slider'),
 	testimonialSwiperPagination: document.querySelector('.js-testimonial-slider-pagination'),
 	modelsSwiperContainer: document.querySelector('.js-models-slider'),
+	form: document.querySelector('.js-form'),
+	formContainer: document.querySelector('.js-form-container'),
+	formPhoneInput: document.querySelector('.js-form-input-phone'),
+	formSubmit: document.querySelector('.js-form-submit'),
 }
 
 
@@ -136,7 +142,52 @@ const createModelSlider = () => createSliderInstance(domSelectors.modelsSwiperCo
 startSlider(createModelSlider, true, createBreakpoint(bp.tablet));
 
 
+// FORM
+// phone number simple validation
+const phoneValid = () => {
+	const input = domSelectors.formPhoneInput;
+	const phoneRe = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+
+	if (input.value.match(phoneRe)) {
+		console.log('true - valid number');
+		return true;
+	} else {
+		console.log("Not a valid Phone Number");
+		return false;
+	}
+}
+
+// showing error and thank you message
+const handleFormValid = (e) => {
+	e.preventDefault();
+
+	const container = domSelectors.formContainer;
+	const form = domSelectors.form;
+	const input = domSelectors.formPhoneInput;
+	const error = document.querySelector('.js-form-error');
+
+	if(phoneValid()) {
+		form.classList.add(state.valid);
+		container.insertAdjacentHTML(
+			'beforeend', 
+			`<p class="banner-callback__thanks">Thank you for your interest, we will give you a callback very soon.</p>`
+		);
+	} else {
+		form.classList.add(state.invalid);
+		input.focus();
+
+		if(!error) {
+			domSelectors.form.insertAdjacentHTML(
+				'afterbegin',
+				`<span class="banner-callback__error js-form-error">Please enter a valid US phone number in the format xxx-xxx-xxxx.</span>`
+			);
+		}
+	}
+}
+
+
 /// Event Listeners
 // Features
 window.addEventListener('load', featuresScroll);
 window.addEventListener('resize', featuresScroll);
+domSelectors.formSubmit.addEventListener('click', handleFormValid);
